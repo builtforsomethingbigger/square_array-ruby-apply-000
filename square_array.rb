d74174181d95a7f9e2fe19057f215effdbe80830
Steve C <stephen.cases@gmail.com>
2020-03-10 14:06:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = [] #empty array
   array.each do |squared| #invoke the each method and assign it to new element
-    squared = squared ** 2
+    squared = squared ** 2 #set new element to the squared version of itself
     new_array << squared
   end
   new_array
